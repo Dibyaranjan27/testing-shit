2b77dde3ee020d727014d8aac4073762e6ad6635
Rutwik Sunil Butani <84628420+Rutwik2003@users.noreply.github.com>
2023-02-11 14:52:58 +0530

    Add files via upload

---
+++ practical1.c
@@ -0,0 +1,12 @@
+//Course: B.Tech CE(Software Engineering)
+//Name: Rutwik Butani
+//Enrollment Number: 202203103510304
+#include <stdio.h>
+void main()
+{
+    printf("##########################\n");
+    printf("Student Name:Rutwik Butani\n");
+    printf("Enrollment Number: 202203103510304\n");
+    printf("Branch:B.Tech CE(Software Engineering\n");
+    printf("##########################\n");
+}
